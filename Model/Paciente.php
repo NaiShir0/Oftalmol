9316f096cf7b2bbc67140076bdceadd433c9432d
NaiShir0 <jnavgar69@gmail.com>
2025-01-29 12:14:20 +0100

    Pacientes php and pacientes table implemented

---
+++ Model/Paciente.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Description of Paciente
+ *
+ * @author Nai
+ */
+
+namespace Plugins\Oftalmol\Model;
+
+use Core\Model\Base;
+
+class Paciente extends Base\ModelClass{
+    use Base\ModelTrait;
+    
+    /**
+     * Strings variables
+     * @var string
+     */
+    public $alergias;
+    public $antecedentes_personales;
+    public $antecedentes_familiares;
+    public $antecedentes_oftalmologicos;
+    public $observaciones;
+    public $nacimiento;
+    public $codgrupo;
+    
+    /**
+     * Primary Key.
+     * Link to Cliente Model.
+     *
+     * @var string
+     */
+    public $codcliente;
+    
+ 
+
+
+    
+    
+    
+    
+    #[\Override]
+    public static function primaryColumn(): string {
+        return 'codcliente';
+    }
+
+    #[\Override]
+    public static function tableName(): string {
+        return 'oft_pacientes';
+    }
+}
