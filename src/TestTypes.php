748c264bd5e903906bfe9a46700b2a8782f59038
José Joaquín García González <jquingarcia@gmail.com>
2025-02-09 12:56:26 +0100

    Implement Refracción
    Se han creado todas las pruebas de este apartado

---
+++ src/TestTypes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FacturaScripts\Plugins\Oftalmol\src;
+
+class TestTypes {
+
+    const TEST_TYPE_AUTOREFRACTOMETER = 1;
+    const TEST_TYPE_SHIASCOPY = 2;
+    const TEST_TYPE_FRONTOFOCOMETER = 3;
+    const TEST_TYPE_REFRACTION = 4;
+}
