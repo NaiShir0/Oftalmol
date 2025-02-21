8fa626669e352e2788e98fdcc2a36086c8333331
José Joaquín García González <jquingarcia@gmail.com>
2025-02-21 13:18:03 +0100

    action: return to expedient

--- Controller/Base/EditTest.php
+++ Controller/Base/EditTest.php
@@ -12,7 +12,7 @@ class EditTest extends EditController {
     protected function createViews() {
         // master view -> EditExpediente in readonly.
         $viewName = Constants::VIEW_EDIT_EXPEDIENT;
-        $this->addEditView($viewName, $this->getModelClassName(),$this->getPageData()['title'],$this->getPageData()['icon']);
+        $this->addEditView($viewName, $this->getModelClassName(), $this->getPageData()['title'], $this->getPageData()['icon']);
 
         // Remove all buttons
         $this->setSettings($viewName, 'btnDelete', false);
@@ -25,7 +25,7 @@ class EditTest extends EditController {
     public function getModelClassName(): string {
         return 'Expedient';
     }
-    
+
     /**
      * Loads the data to display.
      *
@@ -33,12 +33,11 @@ class EditTest extends EditController {
      * @param BaseView $view
      */
     #[\Override]
-    protected function loadData($viewName, $view)
-    {
+    protected function loadData($viewName, $view) {
         if ($viewName === $this->getMainViewName()) {
             parent::loadData($viewName, $view);
             $this->addButton($viewName, [
-                'action' => 'expedient-backward',
+                'action' => 'returnToExpedient',
                 'color' => 'warning',
                 'icon' => 'fas fa-backward',
                 'label' => 'expedient',
@@ -48,9 +47,33 @@ class EditTest extends EditController {
 
         $idexpedient = $this->getViewModelValue($this->getMainViewName(), 'id');
         $where = [
-                new DataBaseWhere('idExpedient', $idexpedient),
-                new DataBaseWhere('idTestType', $view->model->idTestType),
-            ];
+            new DataBaseWhere('idExpedient', $idexpedient),
+            new DataBaseWhere('idTestType', $view->model->idTestType),
+        ];
         $view->loadData('', $where, ['creationDate' => 'DESC']);
+    }
+
+    /**
+     * Run the actions that alter data before reading it.
+     *
+     * @param string $action
+     * @return bool
+     */
+    protected function execPreviousAction($action) {
+        /** Acción para volver al expediente cuando estamos dentro 
+         * de un bloque de pruebas
+         */
+        if ($action === 'returnToExpedient') {
+            $idexpedient = (int) $this->request->get('code', 0);
+            if (false === empty($idexpedient)) {
+                /** TODO: aquí deberíamos poner que cuando volvamos al expediente se quede seleccionado
+                 * el grupo de pruebas del que veníamos.
+                 */
+                $this->redirect('EditExpedient?code=' . $idexpedient, 0);
+                return false;
+            }
+            return true;
+        }
+        return parent::execPreviousAction($action);
     }
 }
